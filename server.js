adb9f371a4e384241b3a3932d5bffe3ee6f00ad8
Kartikey2805 <kartikeygoel093@gmail.com>
2020-08-28 01:00:06 +0530

    prepare to deploy

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 if (process.env.NODE_ENV === 'production') {
   //Set Static folder
   app.use(express.static('client/build'));
-  app.get(
-    '*',
-    ((req, res) = res.sendFile(
-      path.resolve(__dirname, 'client', 'build', 'index.html')
-    ))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
